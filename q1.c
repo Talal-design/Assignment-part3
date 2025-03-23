d7c5390da2b362088dbda18b3140a7054a4dfdd2
Talal Amin <talalamin250@gmail.com>
2025-03-23 11:55:35 +0500

    Header Files and Macro Definition

---
+++ q1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ROWS 1000   // Number of rows in the 2D array
+#define COLS 1000   // Number of columns in the 2D array
+#define NUM_RUNS 10 // Number of times to execute the function for averaging execution time
